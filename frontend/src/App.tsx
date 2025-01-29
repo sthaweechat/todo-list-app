be448cc3d381cf3f92f1dd9e589ae5055299f34d
sthaweechat <sthaweechat@gmail.com>
2025-01-29 08:57:42 +0700

    ทำหน้าการรับค่า

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [task, setTask] = useState<string>("");
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h1>Todo List</h1>
+      <input type="text" value={task} onChange={e => setTask(e.target.value)} placeholder="Add a new task" />
+      <button>Add</button>
+    </div>
+  );
 }
 
-export default App
+export default App;
